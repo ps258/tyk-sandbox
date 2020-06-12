f8ef4d8bcfd742ee6909ca8f5461907ec7571f2c
Peter Stubbs <peter.stubbs@outlook.com>
2020-06-12 20:59:00 +0100

    almost working

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,8 +4,9 @@ echo "Image starting: $1"
 
 if [[ ! -f /initialised ]]
 then
-  echo "[INFO]Setting gateway port in tyk_analytics.conf"
-  set -i "s/TYK_API_PORT/$TYK_API_PORT/g" /opt/tyk-dashboard/tyk_analytics.conf
+  echo "[INFO]Setting gateway host and port in tyk_analytics.conf"
+  sed -i "s/TYK_API_PORT/$TYK_GW_PORT/g" /opt/tyk-dashboard/tyk_analytics.conf
+  sed -i "s/TYK_API_HOST/$TYK_GW_HOST/g" /opt/tyk-dashboard/tyk_analytics.conf
 fi
 
 echo "[INFO]Starting Redis"
@@ -14,25 +15,30 @@ echo "[INFO]Starting Redis"
 echo "[INFO]Starting mongoDB"
 /usr/bin/mongod --fork --logpath /var/log/mongod.log --smallfiles
 
-echo "[INFO]Starting Tyk dashboard"
-/opt/tyk-dashboard/tyk-analytics --conf /opt/tyk-dashboard/tyk_analytics.conf >> /var/log/tyk_dashboard.log 2>&1 &
-sleep 5
-
 if [[ ! -f /initialised ]]
 then
   echo "[INFO]Initialising the dashboard"
   /scripts/init-dashboard.sh
 fi
 
+echo "[INFO]Starting Tyk dashboard"
+cd /opt/tyk-dashboard/
+/opt/tyk-dashboard/tyk-analytics --conf /opt/tyk-dashboard/tyk_analytics.conf >> /var/log/tyk_dashboard.log 2>&1 &
+sleep 5
+
 echo "[INFO]Starting Tyk gateway"
+cd /opt/tyk-gateway/
 /opt/tyk-gateway/tyk --conf /opt/tyk-gateway/tyk.conf >> /var/log/tyk_gateway.log 2>&1 &
 
 echo "[INFO]Starting Tyk pump"
+cd /opt/tyk-pump/
 /opt/tyk-pump/tyk-pump --conf /opt/tyk-pump/pump.conf >> /var/log/tyk_pump.log 2>&1 &
 
 echo "[INFO]Starting Tyk identity broker"
+cd /opt/tyk-identity-broker/
 /opt/tyk-identity-broker/tyk-identity-broker --conf /opt/tyk-tyk-identity-broker/tib.conf >> /var/log/tyk_identity_broker.log 2>&1 &
 
+cd /
 echo "[INFO]Sandbox instance has started"
 sleep 5
 tail -f /var/log/tyk_gateway.log
