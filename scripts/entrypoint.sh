bb3d9b1262e9e4ff72ad9f15bfc9a808d76c16f5
Peter Stubbs <ps258@hotmail.com>
2021-10-29 22:15:49 +0100

    code tidyup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+PATH=/scripts:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH
+
 unalias cp
 
 echo "Image starting: $1"
 
-if [[ ! -f /initialised ]]
-then
-	# copy the config files into place
-	echo "[INFO]Copying the config files into place"
-	if [[ -d /opt/tyk-identity-broker ]]
-	then
-		# TIB is installed, we need to use the tib enabled tyk_analytics.conf
-		cp -f /assets/tyk_analytics-tib.conf /opt/tyk-dashboard/tyk_analytics.conf
-		cp -f /assets/tib.conf /opt/tyk-identity-broker/
-	else
-		cp -f /assets/tyk_analytics.conf /opt/tyk-dashboard
-	fi
-	cp -f /assets/tyk.conf /opt/tyk-gateway/
-	cp -p /assets/pump.conf /opt/tyk-pump/
+if [[ ! -f /initialised ]]; then
+  # copy the config files into place
+  echo "[INFO]Copying the config files into place"
+  if [[ -d /opt/tyk-identity-broker ]]; then
+    # TIB is installed, we need to use the tib enabled tyk_analytics.conf
+    cp -f /assets/tyk_analytics-tib.conf /opt/tyk-dashboard/tyk_analytics.conf
+    cp -f /assets/tib.conf /opt/tyk-identity-broker/
+  else
+    cp -f /assets/tyk_analytics.conf /opt/tyk-dashboard
+  fi
+  cp -f /assets/tyk.conf /opt/tyk-gateway/
+  cp -p /assets/pump.conf /opt/tyk-pump/
   echo "[INFO]Setting gateway host and port in tyk_analytics.conf"
   sed -i "s/SBX_GW_PORT/$SBX_GW_PORT/g" /opt/tyk-dashboard/tyk_analytics.conf
   sed -i "s/SBX_GW_HOST/$SBX_GW_HOST/g" /opt/tyk-dashboard/tyk_analytics.conf
@@ -26,8 +25,7 @@ then
   echo "[INFO]Generating tyk private keys"
   openssl genrsa -out /privkey.pem 2048
   openssl rsa -in /privkey.pem -pubout -out /pubkey.pem
-  if [[ ! -e /opt/tyk-certificates/dashboard-certificate.pem ]]
-  then
+  if [[ ! -e /opt/tyk-certificates/dashboard-certificate.pem ]]; then
     echo "[INFO]Creating certificate for the dashboard and gateway"
     openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout /opt/tyk-certificates/dashboard-key.pem -out /opt/tyk-certificates/dashboard-certificate.pem -subj '/emailAddress=ps258@hotmail.com/C=GB/ST=Mid Lothian/L=Home Office/O=Garage/OU=Desk/CN=example.com'
     ln -s /opt/tyk-certificates/dashboard-certificate.pem /opt/tyk-certificates/gateway-certificate.pem
@@ -48,8 +46,7 @@ echo "[INFO]Starting Tyk dashboard"
 sleep 5
 
 # once the dashboard is running, use the admin API to create users and upload a licence
-if [[ ! -f /initialised ]]
-then
+if [[ ! -f /initialised ]]; then
   echo "[INFO]Initialising the dashboard"
   /scripts/init-dashboard.sh
 fi
@@ -60,8 +57,7 @@ echo "[INFO]Starting Tyk gateway"
 echo "[INFO]Starting Tyk pump"
 /scripts/start-pump.sh
 
-if [[ -d /opt/tyk-identity-broker/ ]]
-then
+if [[ -d /opt/tyk-identity-broker/ ]]; then
   echo "[INFO]Starting Tyk identity broker"
   /scripts/start-tib.sh
 fi
--- scripts/init-dashboard.sh
+++ scripts/init-dashboard.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+# if SBX_LICENSE is set, install the licence
+# if SBX_USER and SBC_PASSWORD are set, create an admin user with those credentials
+# saves the admin key for an admin user into /initial_credentials.txt as well as username and password
+# also publishes all APIs from /assets/APIs/*.json if an admin account was created
+
+PATH=/scripts:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH
 PROTOCOL=http
 
 if [[ -n $SBX_LICENSE ]]
 then
   echo "[INFO]Adding dashboard license"
-  curl -k -s -d "license=$SBX_LICENSE" -X POST $PROTOCOL://localhost:3000/license
+  curl -k -s -d "license=$SBX_LICENSE" $PROTOCOL://localhost:3000/license
   echo Initial licence: $SBX_LICENSE >> /initial_credentials.txt
 fi
 
 if [[ -n $SBX_USER && -n $SBX_PASSWORD ]]
 then
-  SBX_PASSWORD=$(echo $SBX_PASSWORD | base64 -d)
+  SBX_CLEAR_PASSWORD=$(echo $SBX_PASSWORD | base64 -d)
   echo "[INFO]Bootstraping default user: $SBX_USER"
-  curl -k -s -d "email_address=$SBX_USER&first_name=Tyk&last_name=Admin&password=$SBX_PASSWORD&confirm_password=$SBX_PASSWORD" -X POST $PROTOCOL://localhost:3000/bootstrap
+  curl -k -s -d "email_address=$SBX_USER&first_name=Tyk&last_name=Admin&password=$SBX_CLEAR_PASSWORD&confirm_password=$SBX_CLEAR_PASSWORD" $PROTOCOL://localhost:3000/bootstrap
   echo Initial admin User: $SBX_USER >> /initial_credentials.txt
-  echo Initial admin Password: $SBX_PASSWORD >> /initial_credentials.txt
-	echo Initial admin key: $(/scripts/admin-auth) >> /initial_credentials.txt
+  echo Initial admin Password (base64 encoded): $SBX_PASSWORD >> /initial_credentials.txt
+	echo Initial admin key: $(admin-auth) >> /initial_credentials.txt
+  # publish all apis from /assets/APIs
+  /scripts/publish-apis /assets/APIs/*.json
 fi
-
-/scripts/publish-apis /assets/APIs/*.json
-
 touch /initialised 
