bb6ae5f97b80c475c166daaf2842c90778b0f6bc
Peter Stubbs <ps258@hotmail.com>
2022-02-01 14:08:47 +0000

    adding the start of mdcb support

---
+++ scripts/start-mdcb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PATH=/scripts:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH
+
+command=/opt/tyk-sink/tyk-sink
+conf=/opt/tyk-sink/tyk_sink.conf
+log=/var/log/tyk-sink.log
+ci $conf
+
+# refuse to start if its already running
+if pgrep -x $(basename $command) > /dev/null
+then
+  echo "[FATAL]$command running. Not starting another";
+  exit 1
+fi
+
+# startup
+$command -conf $conf &>> $log &
+err=$?
+sleep 1
+
+# check if its running
+if ! pgrep -x $(basename $command) > /dev/null
+then
+  echo "[FATAL]$command exited with $err and didn't start" 1>&2
+  echo "[FATAL]Check $log for errors" 1>&2
+  exit 1
+else
+  echo "[INFO]$command started"
+fi
