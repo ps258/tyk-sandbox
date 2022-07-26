3e0b7275049f102a8df33fdccda549ea8130e95a
Peter Stubbs <peter@tyk.io>
2022-07-26 11:39:46 +0100

    start-redis.sh,stop-redis.sh: able to take a config file and start/stop the appropriate process

--- scripts/start-redis.sh
+++ scripts/start-redis.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
 
+# script to start redis. 
+# Can take a single parameter which is the config file to use
+# finds the port in the config file and uses that to check if it is already in use
+
 PATH=/scripts:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH
 
 command=/usr/bin/redis-server
+TRUE=0
+FALSE=1
+PID=
+
+function isRunning {
+  # check if there's a process listening on the expected port
+	# also populates $PID
+  PID=$(ss -lnptu4H  "( sport = :$PORT )" | awk '/pid=/ {print $NF}' | cut -d, -f 2 | cut -d= -f 2)
+  if [[ ! -z $PID ]]
+  then
+    return $TRUE
+  else
+    return $FALSE
+  fi
+}
+
+if [[ $# > 0 ]]; then
+  conf="$1"
+  if [[ ! -f "$conf" ]]; then
+    echo "[FATAL]Config file not found '$conf'"
+    exit 1
+  fi
+else
+  conf=/etc/redis.conf
+fi
+
+PORT=$(awk '/^port/ {print $NF}' "$conf")
 
 # refuse to start if its already running
-if [[ $(pgrep -x $(basename $command) | wc -l) -gt 0 ]]
+
+if isRunning
 then
-  echo "[FATAL]$command running. Not starting";
+  echo "[FATAL]$command already running on port $PORT. Not starting. PID=$PID";
   exit 1
 fi
 
 # startup
 mkdir -p /var/lib/redis/
-conf=/etc/redis.conf
-ci $conf
-$command $conf --daemonize yes
+ci "$conf"
+$command "$conf" --daemonize yes
 
 # check if its running
-if [[ $(pgrep -x $(basename $command) | wc -l) -lt 1 ]]
+if isRunning
 then
-  echo "[FATAL]$(basename $command) didn't start"
+  echo "[INFO]'$command "$conf" --daemonize yes' started. PID=$PID"
+  exit 0
+else  
+  echo "[FATAL]'$command "$conf" --daemonize yes' didn't start"
   exit 1
-else
-  echo "[INFO]$(basename $command) started"
 fi
--- scripts/stop-redis.sh
+++ scripts/stop-redis.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
+# script to stop redis. 
+# Can take a single parameter which is the config file to use
+# finds the port in the config file and uses that to check if it is already in use
+
 PATH=/scripts:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH
 
-process=redis-server
+TRUE=0
+FALSE=1
+PID=
 count=0
 maxWait=11
 
-while pgrep -x $process > /dev/null
+function isRunning {
+  # check if there's a process listening on the expected port
+	# also populates $PID
+  PID=$(ss -lnptu4H  "( sport = :$PORT )" | awk '/pid=/ {print $NF}' | cut -d, -f 2 | cut -d= -f 2)
+  if [[ ! -z $PID ]]
+  then
+    return $TRUE
+  else
+    return $FALSE
+  fi
+}
+
+if [[ $# > 0 ]]; then
+  conf="$1"
+  if [[ ! -f "$conf" ]]; then
+    echo "[FATAL]Config file not found '$conf'"
+    exit 1
+  fi
+else
+  conf=/etc/redis.conf
+fi
+
+PORT=$(awk '/^port/ {print $NF}' "$conf")
+
+while isRunning
 do
   if [[ $(( count % 5 )) == 0 ]]
   then
-    echo "[INFO]Killing $process"
-    pkill -x $process
+    echo "[INFO]Killing $PID listening on port $PORT"
+    kill $PID
   fi
   if [[ $count -gt 1 && $(( count % $maxWait )) == 0 ]]
   then
-    echo "[FATAL]$process still running after ${maxWait}s. Unable to kill. Investigate"
+    echo "[FATAL]$PID still running after ${maxWait}s. Unable to kill. Investigate"
     exit 1
   fi
   sleep 1
@@ -23,5 +53,5 @@ do
 done
 if [[ $count -eq 0 ]]
 then
-  echo "[INFO]$process not running. Nothing to kill"
+  echo "[INFO]Nothing listening on port $PORT. Nothing to kill"
 fi
