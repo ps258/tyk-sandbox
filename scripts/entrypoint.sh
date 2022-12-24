54d30328aeaeabc2cf5e3e7ad5e8a3f1524fd40b
Peter Stubbs <peter@tyk.io>
2022-12-24 14:21:50 +0000

    entrypoint.sh: add a SAN to the generated certificate

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,16 +6,16 @@ echo "Image starting: $1"
 
 if [[ ! -f /initialised ]]; then
 
-  echo "[INFO]Copying the config files into place"
-  if [[ -d /opt/tyk-identity-broker ]]; then
-    # TIB is installed, we need to use the tib enabled tyk_analytics.conf
-    cp -f /assets/tyk_analytics-tib.conf /opt/tyk-dashboard/tyk_analytics.conf
-    cp -f /assets/tib.conf /opt/tyk-identity-broker/
-  else
-    cp -f /assets/tyk_analytics.conf /opt/tyk-dashboard
-  fi
-  cp -f /assets/tyk.conf /opt/tyk-gateway/
-  cp -f /assets/pump.conf /opt/tyk-pump/
+	echo "[INFO]Copying the config files into place"
+	if [[ -d /opt/tyk-identity-broker ]]; then
+		# TIB is installed, we need to use the tib enabled tyk_analytics.conf
+		cp -f /assets/tyk_analytics-tib.conf /opt/tyk-dashboard/tyk_analytics.conf
+		cp -f /assets/tib.conf /opt/tyk-identity-broker/
+	else
+		cp -f /assets/tyk_analytics.conf /opt/tyk-dashboard
+	fi
+	cp -f /assets/tyk.conf /opt/tyk-gateway/
+	cp -f /assets/pump.conf /opt/tyk-pump/
 
 	if [[ -n $SBX_GW_CNAME ]]; then
 		SBX_GW_HOST=$SBX_GW_CNAME
@@ -24,23 +24,51 @@ if [[ ! -f /initialised ]]; then
 		SBX_DSHB_HOST=$SBX_DSHB_CNAME
 	fi
 
-  echo "[INFO]Setting gateway host and port in tyk_analytics.conf"
-  sed -i "s/SBX_GW_PORT/$SBX_GW_PORT/g" /opt/tyk-dashboard/tyk_analytics.conf
-  sed -i "s/SBX_GW_HOST/$SBX_GW_HOST/g" /opt/tyk-dashboard/tyk_analytics.conf
-  sed -i "s/SBX_DSHB_HOST/$SBX_DSHB_HOST/g" /opt/tyk-dashboard/tyk_analytics.conf
-  sed -i "s/SBX_DSHB_PORT/$SBX_DSHB_PORT/g" /opt/tyk-dashboard/tyk_analytics.conf
+	echo "[INFO]Setting gateway host and port in tyk_analytics.conf"
+	sed -i "s/SBX_GW_PORT/$SBX_GW_PORT/g" /opt/tyk-dashboard/tyk_analytics.conf
+	sed -i "s/SBX_GW_HOST/$SBX_GW_HOST/g" /opt/tyk-dashboard/tyk_analytics.conf
+	sed -i "s/SBX_DSHB_HOST/$SBX_DSHB_HOST/g" /opt/tyk-dashboard/tyk_analytics.conf
+	sed -i "s/SBX_DSHB_PORT/$SBX_DSHB_PORT/g" /opt/tyk-dashboard/tyk_analytics.conf
 
-  echo "[INFO]Generating tyk private keys"
-  openssl genrsa -out /privkey.pem 2048
-  openssl rsa -in /privkey.pem -pubout -out /pubkey.pem
+	if [[ ! -e /privkey.pem ]]; then
+		echo "[INFO]Generating tyk private keys to secure traffic between the dashboard and the management gateway"
+		openssl genrsa -out /privkey.pem 2048
+		openssl rsa -in /privkey.pem -pubout -out /pubkey.pem
+	fi
 
 	# create a local cert if one isn't already present
-  if [[ ! -e /opt/tyk-certificates/dashboard-certificate.pem ]]; then
-    echo "[INFO]Creating certificate for the dashboard and gateway"
-    openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout /opt/tyk-certificates/dashboard-key.pem -out /opt/tyk-certificates/dashboard-certificate.pem -subj '/emailAddress=email@company.com/C=GB/ST=Mid Lothian/L=Home Office/O=Garage/OU=Desk/CN=tyk-sandbox.com'
-    ln -s /opt/tyk-certificates/dashboard-certificate.pem /opt/tyk-certificates/gateway-certificate.pem
-    ln -s /opt/tyk-certificates/dashboard-key.pem /opt/tyk-certificates/gateway-key.pem
-  fi
+	if [[ ! -e /opt/tyk-certificates/dashboard-certificate.pem ]]; then
+		echo "[INFO]Creating certificate for the dashboard and gateway"
+		SAN="DNS:localhost"
+		if [[ -n $SBX_GW_CNAME ]]; then
+			SAN="DNS:$SBX_GW_CNAME, $SAN"
+			SUBJECT="/emailAddress=email@company.com/C=GB/ST=Mid Lothian/L=Home Office/O=Garage/OU=Desk/CN=$SBX_GW_CNAME"
+		else
+			SUBJECT='/emailAddress=email@company.com/C=GB/ST=Mid Lothian/L=Home Office/O=Garage/OU=Desk/CN=tyk-sandbox.com'
+		fi
+		if [[ -n $SBX_DSHB_CNAME ]]; then
+			SAN="DNS:$SBX_DSHB_CNAME, $SAN"
+		fi
+		if [[ -n $SBX_PTL_CNAME ]]; then
+			SAN="DNS:$SBX_PTL_CNAME, $SAN"
+		fi
+		openssl genrsa -des3 -passout pass:ABC-123 -out /opt/tyk-certificates/dashboard-key.pem 2048
+		openssl req -new -key /opt/tyk-certificates/dashboard-key.pem -passin pass:ABC-123 -subj "$SUBJECT" -out /opt/tyk-certificates/certificate.csr
+		cp /opt/tyk-certificates/dashboard-key.pem /opt/tyk-certificates/dashboard-key.pem.orig
+		openssl rsa -in /opt/tyk-certificates/dashboard-key.pem.orig -out /opt/tyk-certificates/dashboard-key.pem -passin pass:ABC-123
+		rm -f /opt/tyk-certificates/dashboard-key.pem.orig
+		cat > v3.ext <<- EOF
+		subjectKeyIdentifier   = hash
+		authorityKeyIdentifier = keyid:always,issuer:always
+		basicConstraints       = CA:TRUE
+		keyUsage               = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign
+		subjectAltName         = DNS:kgw.stubbs-family.org, DNS:*.stubbs-family.org, DNS:localhost
+		issuerAltName          = issuer:copy
+		EOF
+		openssl x509 -req -in /opt/tyk-certificates/certificate.csr -signkey /opt/tyk-certificates/dashboard-key.pem -out /opt/tyk-certificates/dashboard-certificate.pem -days 3650 -sha256 -extfile v3.ext
+		ln -s /opt/tyk-certificates/dashboard-certificate.pem /opt/tyk-certificates/gateway-certificate.pem
+		ln -s /opt/tyk-certificates/dashboard-key.pem /opt/tyk-certificates/gateway-key.pem
+	fi
 fi
 
 #grep -q $SBX_GW_HOST /etc/hosts || echo "127.0.0.1 $SBX_GW_HOST" >> /etc/hosts
@@ -60,8 +88,8 @@ sleep 1
 
 # once the dashboard is running, use the admin API to create users and upload a licence
 if [[ ! -f /initialised ]]; then
-  echo "[INFO]Initialising the dashboard"
-  /scripts/init-dashboard.sh > /var/log/init-dashboard.log 2>&1
+	echo "[INFO]Initialising the dashboard"
+	/scripts/init-dashboard.sh > /var/log/init-dashboard.log 2>&1
 	sleep 1
 	restart dashboard
 	sleep 1
@@ -74,13 +102,13 @@ echo "[INFO]Starting Tyk pump"
 /scripts/start-pump.sh
 
 if [[ -d /opt/tyk-identity-broker/ ]]; then
-  echo "[INFO]Starting Tyk identity broker"
-  /scripts/start-tib.sh
+	echo "[INFO]Starting Tyk identity broker"
+	/scripts/start-tib.sh
 fi
 
 if [[ -d /opt/tyk-sink ]]; then
-  echo "[INFO]Starting MDCB"
-  start mdcb
+	echo "[INFO]Starting MDCB"
+	start mdcb
 fi
 
 #echo "[INFO]Capping analytics collections"
