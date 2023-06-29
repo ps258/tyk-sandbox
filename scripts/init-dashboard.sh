b841fa393cfdedf9c1a6e5520176342fa76f18b6
Peter Stubbs <peter@tyk.io>
2023-06-29 15:05:55 +0100

    scripts/entrypoint.sh scripts/init-dashboard.sh: moved the updating of /etc/hosts from init-dashboard.sh to entrypoint.sh so that it happens at every container startup

--- scripts/init-dashboard.sh
+++ scripts/init-dashboard.sh
@@ -78,19 +78,5 @@ then
   # publish all apis from /assets/APIs
   /scripts/publish-apis /assets/APIs/*.json
 fi
-if [[ -n $SBX_HOST_ENTRIES ]]; then
-	# setup any extra entries in /etc/hosts that are provided via $SBX_HOST_ENTRIES
-	# The format is for it is # the format is 'ipaddress:alias:alias,next ipaddress:its alias:its other alias'
-	# like this
-	# SBX_HOST_ENTRIES=$SBX_HOST_IP_ADDR:httpbin.org:homebin.org,$SBX_DSHB_HOST:dbhost.home:anotherhost.home
-	# this entry would go in your ~/.tyk-sandbox
-	# Note that $SBX_HOST_IP_ADDR will be the IP address of the host running the sandbox not the ip address of the sandbox itself
-	echo "### added during setup $0 ###" >> /etc/hosts
-	for line in $(eval echo $SBX_HOST_ENTRIES | sed 's/,/ /g'); do  echo $line | sed 's/:/ /g' | \
-		while read address names; do
-			echo $address $names >> /etc/hosts
-		done
-	done
-fi
 
 touch /initialised 
