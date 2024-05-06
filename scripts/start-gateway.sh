47b4f4ae729f844740652f59b4f94cb5593aee82
Peter Stubbs <peter@tyk.io>
2024-05-06 15:10:21 +0100

    scripts/start-gateway.sh: removed tabs and used jq

--- scripts/start-gateway.sh
+++ scripts/start-gateway.sh
@@ -12,37 +12,37 @@ FALSE=1
 PID=
 
 function isRunning {
-	# check if there's a process listening on the expected port
-	# also populates $PID
-	PID=$(ss -lnptuH  "( sport = :$PORT )" | awk '/pid=/ {print $NF}' | cut -d, -f 2 | cut -d= -f 2 | sort -u)
-	if [[ ! -z $PID ]]
-	then
-		return $TRUE
-	else
-		return $FALSE
-	fi
+  # check if there's a process listening on the expected port
+  # also populates $PID
+  PID=$(ss -lnptuH  "( sport = :$PORT )" | awk '/pid=/ {print $NF}' | cut -d, -f 2 | cut -d= -f 2 | sort -u)
+  if [[ ! -z $PID ]]
+  then
+    return $TRUE
+  else
+    return $FALSE
+  fi
 }
 
 if [[ $# > 0 ]]; then
-	conf="$1"
-	if [[ ! -f "$conf" ]]; then
-		echo "[FATAL]Config file not found '$conf'"
-		exit 1
-	fi
-	PORT=$(awk '/"listen_port"/ {print $NF}' $conf | cut -d: -f2 | sed -e 's/,//')
-	log=/var/log/tyk-gateway-rpc-$PORT.log
+  conf="$1"
+  if [[ ! -f "$conf" ]]; then
+    echo "[FATAL]Config file not found '$conf'"
+    exit 1
+  fi
+  PORT=$(jq -r .listen_port $conf)
+  log=/var/log/tyk-gateway-$PORT.log
 else
-	conf=/opt/tyk-gateway/tyk.conf
-	PORT=$(awk '/"listen_port"/ {print $NF}' $conf | cut -d: -f2 | sed -e 's/,//')
-	log=/var/log/tyk-gateway.log
+  conf=/opt/tyk-gateway/tyk.conf
+  PORT=$(awk '/"listen_port"/ {print $NF}' $conf | cut -d: -f2 | sed -e 's/,//')
+  log=/var/log/tyk-gateway.log
 fi
 ci $conf
 
 # refuse to start if its already running
 if isRunning
 then
-	echo "[FATAL]$command already running on port $PORT. Not starting. PID=$PID";
-	exit 1
+  echo "[FATAL]$command already running on port $PORT. Not starting. PID=$PID";
+  exit 1
 fi
 
 # startup
@@ -53,10 +53,10 @@ sleep 1
 # check if its running
 if isRunning
 then
-	echo "[INFO]'$command --conf $conf' started. PID=$PID"
-	exit 0
+  echo "[INFO]'$command --conf $conf' started. PID=$PID"
+  exit 0
 else  
-	echo "[FATAL]'$command --conf $conf' didn't start"
-	echo "[FATAL]Check $log for errors"
-	exit 1
+  echo "[FATAL]'$command --conf $conf' didn't start"
+  echo "[FATAL]Check $log for errors"
+  exit 1
 fi
