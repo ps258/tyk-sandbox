1bec6b124cc414849f0296fc9d11677f708e709e
Peter Stubbs <peter@tyk.io>
2025-03-14 11:05:46 +0000

    scripts/upgrade-dashboard.sh scripts/upgrade-gateway.sh scripts/upgrade-pump.sh: scripts to inplace upgrade dashboard, gateway and pump

---
+++ scripts/upgrade-gateway.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SERVICE=gateway
+PRODUCT=tyk-$SERVICE
+BINARY=tyk
+BASEDIR=/opt/$PRODUCT
+if [[ $# -lt 1 ]]
+then
+  echo Must provide $SERVICE version like 5.7.2
+  exit 1
+fi
+
+if [[ -f $BASEDIR/$PRODUCT-$1 ]]
+then
+  # version alread installed, link it and restart
+  ln -sf $BASEDIR/$PRODUCT-$1 $BASEDIR/$BINARY
+  restart $SERVICE
+else
+  yum -y install $PRODUCT-$1
+  if [[ -f $BASEDIR/$BINARY ]]
+  then
+    mv $BASEDIR/$BINARY $BASEDIR/$PRODUCT-$1
+    ln -sf $BASEDIR/$PRODUCT-$1 $BASEDIR/$BINARY
+    restart $SERVICE
+  fi
+fi
