5811fa11bff3e46263d6aa0d29846b57174b6d5f
Peter Stubbs <peter.stubbs@outlook.com>
2025-04-16 21:46:37 +0100

    scripts/enable-hybrid.sh

--- scripts/enable-hybrid.sh
+++ scripts/enable-hybrid.sh
@@ -8,8 +8,8 @@ ADMIN_KEY=$(jq -r .admin_secret /opt/tyk-dashboard/tyk_analytics.conf)
 
 for ORG_ID in $(curl -s http://localhost:3000/admin/organisations -H "Admin-Auth: $ADMIN_KEY" | jq -r .organisations[].id)
 do
-	echo "[INFO]Enabling hybrid on org_id $ORG_ID"
-	org=$(curl -s http://localhost:3000/admin/organisations/$ORG_ID -H "Admin-Auth: $ADMIN_KEY" | jq ".hybrid_enabled = true")
-	# echo $org | jq .
-	curl -sX PUT http://localhost:3000/admin/organisations/$ORG_ID -H "Admin-Auth: $ADMIN_KEY" -d "$org"
+    echo "[INFO]Enabling hybrid mode on org_id $ORG_ID"
+    org=$(curl -s http://localhost:3000/admin/organisations/$ORG_ID -H "Admin-Auth: $ADMIN_KEY" | jq ".hybrid_enabled = true")
+    # echo $org | jq .
+    curl -sX PUT http://localhost:3000/admin/organisations/$ORG_ID -H "Admin-Auth: $ADMIN_KEY" -d "$org"
 done
